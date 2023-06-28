5ee37d9e1f847284d543fdc5b0cbd6b12567bf4b
NucleonGodX <racerpro41@gmail.com>
2023-06-28 16:49:50 +0530

    footer added

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import Footer from "./Footer";
 export default function Layout(){
     return(<><Header/>
-    <Outlet/></>)
+    <main className="main-content">
+    <Outlet/></main>
+    <Footer/></>)
 }
