411026ee8a1b73c7cfcd8c174152dd27015be9fe
NucleonGodX <racerpro41@gmail.com>
2023-07-05 10:58:27 +0530

    articles added with basic styling

--- src/pages/About-us.jsx
+++ src/pages/About-us.jsx
@@ -5,10 +5,10 @@ export default function About(){
     const [data, setData]=React.useState(Data)
     const dataItem=data.map((info)=> 
     
-    <Link to={`/about-us/${info.title}`} className="about-cards"><div>
-        <img src={info.image}/>
-        <h1 className="info-title">{info.title}</h1>
-        <h2 className="info-info">{info.info}</h2>
+    <Link to={`/about-us/${info.title}`} className="about-cards hover:w-96 shadow-2xl hover:bg-violet-400	hover:font-bold		 	" ><div >
+        <img src={info.image} className="hover:w-96"/>
+        <h1 className="info-title hover:text-pink-700	">{info.title}</h1>
+        <h2 className="info-info text-slate-950		">{info.info}</h2>
         </div></Link>)
     return(<>
         <h1 className="about-heading">MORE PERKS FOR YOUR ASSISTANCE</h1>
--- src/pages/PerkPages/Diet.jsx
+++ src/pages/PerkPages/Diet.jsx
@@ -1,4 +1,15 @@
 import React from "react";
 export default function Diet(){
-    return(<h1>Diet Article</h1>)
+    return(<div className="diet-content"><h1 className="diet-heading">Lol</h1>
+    <h1 className="diet-article">In today's fast-paced world, maintaining a healthy diet is more important than ever. A well-balanced diet not only supports optimal physical health but also contributes to overall well-being. However, with countless diet trends and conflicting information available, it can be overwhelming to create a diet plan that works for you. In this comprehensive guide, we will walk you through the essential steps to design a personalized and effective diet plan that suits your needs and goals.
+    <br/><br/><h1 className="step-style">Step 1</h1>
+    <div className="flex justify-center "><img className="w-96 rounded-3xl"src="https://images.unsplash.com/photo-1581291518857-4e27b48ff24e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cGxhbm5pbmd8ZW58MHx8MHx8fDA%3D&w=1000&q=80"/>
+     Assess Your Goals and Current Health Status
+    The first step in creating a successful diet plan is to define your goals. Are you looking to lose weight, build muscle, improve your energy levels, or simply enhance your overall health? Identifying your objectives will help you tailor your diet accordingly. Additionally, consider your current health status and any specific dietary requirements you may have, such as food allergies or intolerances. It is always advisable to consult with a healthcare professional or registered dietitian to ensure your diet plan aligns with your individual needs.
+    </div><br/><br/>
+    <h1 className="step-style">Step 1</h1>
+    <div className="flex"><img className="w-96 rounded-3xl"src="https://images.unsplash.com/photo-1581291518857-4e27b48ff24e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cGxhbm5pbmd8ZW58MHx8MHx8fDA%3D&w=1000&q=80"/>
+     Assess Your Goals and Current Health Status
+    The first step in creating a successful diet plan is to define your goals. Are you looking to lose weight, build muscle, improve your energy levels, or simply enhance your overall health? Identifying your objectives will help you tailor your diet accordingly. Additionally, consider your current health status and any specific dietary requirements you may have, such as food allergies or intolerances. It is always advisable to consult with a healthcare professional or registered dietitian to ensure your diet plan aligns with your individual needs.
+    </div><br/><br/></h1></div>)
 }
