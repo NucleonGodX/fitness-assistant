5b483c8e0678a1c556b11f062f1e554092f19382
NucleonGodX <racerpro41@gmail.com>
2023-07-12 22:51:29 +0530

    model added for body part selection

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 export default function Header(){
     return(        <div className='topnav'>
     <NavLink to='/'>FitACAD</NavLink>
-    <NavLink to="/exercises" className={({isActive})=>isActive?"after-click": "before"}>Exercises</NavLink>
+    <NavLink to="/Exercises" className={({isActive})=>isActive?"after-click": "before"}>Exercises</NavLink>
     <div className="topnav-right">
     <NavLink to= "/about-us" className={({isActive})=>isActive?"after-click": "before"}>About Us</NavLink>
     <NavLink to="/sign-up" className={({isActive})=>isActive?"after-click": "before"}>Sign Up</NavLink>
--- src/pages/Exercises.jsx
+++ src/pages/Exercises.jsx
@@ -1,4 +1,16 @@
-import React from "react";
+import { OrbitControls } from "@react-three/drei";
+import { Model } from "../components/model";
+import { Canvas } from "@react-three/fiber";
+import { AmbientLight } from "three";
+
 export default function Exercises(){
-    return(<h1>Exerccc</h1>)
+  return(<div className="canvas-container"><Canvas shadows camera={{position: [0,2,5], fov:30}}>
+    <color attach="background" args={["#ececec"]}/>
+  <OrbitControls/>
+  <group position-y={-1}>
+    <Model/>
+    </group>
+    <ambientLight intensity={1}/>
+</Canvas></div>
+    )
 }
